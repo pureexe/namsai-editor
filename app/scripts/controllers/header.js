8b45e413766647e065383229d7be386d33ad5f4d
Pakkapon Phongthawee <me@pureapp.in.th>
2016-09-05 07:14:38 +0700

    draft: navbar

---
+++ app/scripts/controllers/header.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name namsaiEditorApp.controller:HeaderCtrl
+ * @description
+ * # HeaderCtrl
+ * Controller of the namsaiEditorApp
+ */
+angular.module('namsaiEditorApp')
+  .controller('HeaderCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
