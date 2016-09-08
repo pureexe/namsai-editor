33555730fd29693046c6402e7dcb01589ff16994
Pakkapon Phongthawee <me@pureapp.in.th>
2016-09-08 19:18:09 +0700

    list must working (?)

--- app/scripts/controllers/user.js
+++ app/scripts/controllers/user.js
@@ -8,7 +8,7 @@
  * Controller of the namsaiEditorApp
  */
 angular.module('namsaiEditorApp')
-  .controller('UserCtrl', function (API,$http,$routeParams,$scope) {
+  .controller('UserCtrl', function (API,$http,$routeParams,$scope,localStorageService) {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
@@ -21,11 +21,29 @@ angular.module('namsaiEditorApp')
         method: "GET",
         headers: {'Content-Type': 'application/x-www-form-urlencoded'}
       }).then(function(response) {
-        console.log(response);
         $scope.user.name = response.data.name;
         $scope.user.bio = response.data.bio;
         $scope.user.username = response.data.username;
       },function(response) {
+        if(response.status == 404){
+          $scope.notFoundUser = true;
+        }
+      });
+      var getRepoURL = API+'/v1/users/'+username+'/repos';
+      var token = "";
+      if(token = localStorageService.get("access_token")){
+        getRepoURL+="?access_token="+token;
+      }
+      $http({
+        url: getRepoURL,
+        method: "GET",
+        headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+      }).then(function(response) {
+        $scope.repos = response.data;
+      },function(response) {
+        if(response.status == 404){
+          $scope.notFoundUser = true;
+        }
       });
     };
     initial();
