ce57efe74ddf1c63bf78369f8614704636e1887a
Pakkapon Phongthawee <me@pureapp.in.th>
2016-09-09 13:34:36 +0700

    WIP : train.js

--- app/scripts/controllers/train.js
+++ app/scripts/controllers/train.js
@@ -15,7 +15,48 @@ angular.module('namsaiEditorApp')
       'Karma'
     ];
     $scope.appname = $routeParams.repo;
-    var initial = function(){
+    var currentTopicPosition;
+    var getStoryList = function(){
+      var getStoryURL = API+'/v1/repos/'+$routeParams.user+'/'+$routeParams.repo+'/stories';
+      var token = "";
+      if(token = localStorageService.get("access_token")){
+        getStoryURL+="?access_token="+token;
+      }
+      $http({
+        url: getStoryURL,
+        method: "GET",
+        headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+      }).then(function(response) {
+        if(response.data.stories){
+          $scope.stories = response.data.stories;
+        }
+      },function(response) {
+        if(response.status == 404){
+          $scope.notFoundRepo = true;
+        }
+      });
+    }
+    var getStory = function(storyId){
+      var getStoryURL = API+'/v1/repos/'+$routeParams.user+'/'+$routeParams.repo+'/stories/'+storyId;
+      var token = "";
+      if(token = localStorageService.get("access_token")){
+        getStoryURL+="?access_token="+token;
+      }
+      $http({
+        url: getStoryURL,
+        method: "GET",
+        headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+      }).then(function(response) {
+        if(response.data.id){
+          $scope.topic = response.data;
+        }
+      },function(response) {
+        if(response.status == 404){
+          $scope.notFoundRepo = true;
+        }
+      });
+    }
+    var getRepo = function(){
       var getRepoURL = API+'/v1/repos/'+$routeParams.user+'/'+$routeParams.repo;
       var token = "";
       if(token = localStorageService.get("access_token")){
@@ -26,12 +67,26 @@ angular.module('namsaiEditorApp')
         method: "GET",
         headers: {'Content-Type': 'application/x-www-form-urlencoded'}
       }).then(function(response) {
-        console.log(response);
       },function(response) {
         if(response.status == 404){
           $scope.notFoundRepo = true;
         }
       });
     }
+    var initial = function(){
+      getRepo();
+      getStoryList();
+    }
     initial();
+    $scope.chooseStory = function(storyId){
+      for(var i=0;i<$scope.stories.length;i++){
+        if($scope.stories[i].id == storyId){
+          currentTopicPosition = i;
+        }
+      }
+      getStory(storyId);
+    }
+    $scope.topicNameChange = function(){
+      $scope.stories[currentTopicPosition].name = $scope.topic.name;
+    }
   });
